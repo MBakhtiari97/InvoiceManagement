abea7351896fe21c4cd4eb0fc534c351641094d5
mohammadba86 <mohammadba86@yahoo.com>
2022-07-13 18:48:50 +0430

    Executing Authorization Requirements

---
+++ InvoiceManagement/Authorization/InvoiceOperations.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+using System.ComponentModel;
+
+namespace InvoiceManagement.Authorization
+{
+    public class InvoiceOperations
+    {
+        public static OperationAuthorizationRequirement Create =
+            new OperationAuthorizationRequirement
+            {
+                Name = Constants.CreateOperationName
+            };
+
+        public static OperationAuthorizationRequirement Read =
+            new OperationAuthorizationRequirement
+            {
+                Name = Constants.ReadOperationName
+            };
+
+        public static OperationAuthorizationRequirement Update =
+            new OperationAuthorizationRequirement
+            {
+                Name = Constants.UpdateOperationName
+            };
+
+        public static OperationAuthorizationRequirement Delete =
+            new OperationAuthorizationRequirement
+            {
+                Name = Constants.DeleteOperationName
+            };
+
+        public static OperationAuthorizationRequirement Approved =
+            new OperationAuthorizationRequirement
+            {
+                Name = Constants.ApprovedOperationName
+            };
+
+        public static OperationAuthorizationRequirement Rejected =
+            new OperationAuthorizationRequirement
+            {
+                Name = Constants.RejectedOperationName
+            };
+    }
+
+    public class Constants
+    {
+        public static readonly string CreateOperationName = "Create";
+        public static readonly string ReadOperationName = "Read";
+        public static readonly string UpdateOperationName = "Update";
+        public static readonly string DeleteOperationName = "Delete";
+
+        public static readonly string ApprovedOperationName = "Approved";
+        public static readonly string RejectedOperationName = "Rejected";
+
+
+    }
+}
--- InvoiceManagement/Pages/Index.cshtml.cs
+++ InvoiceManagement/Pages/Index.cshtml.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace InvoiceManagement.Pages
 {
+    [AllowAnonymous]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
